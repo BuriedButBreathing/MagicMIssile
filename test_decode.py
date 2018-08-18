2c7c385dc683d61bd170dd0e427e6a5d342b84f8
BuriedButBreathing <higgins.christopher.j@gmail.com>
2018-08-18 14:54:41 -0400

    Update

--- test_decode.py
+++ test_decode.py
@@ -1,14 +1,15 @@
 import base64
-print "Select input encoding:"
-print "A: Base64"
-print "B: Hex"
-print "C: Decimal"
+print """Select which encoding the input string is in:
+A: Base64
+B: Hex
+C: Decimal """
 selection = raw_input()
-#if selection = A
+print selection
+# if selection = "A"
 #    start = raw_input('Pase Base64 string to be decoded: ' )
 #    result = base64.b64decode(start)
 #if selection = B
 #if selection = C
 #start = raw_input('Paste base64 string to be decoded: ' )
 #result = base64.b64decode(start)
-print result
+#print result
