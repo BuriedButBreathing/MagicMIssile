0e5ca21f9be910b1eed37c6b0cff87b5186dd559
BuriedButBreathing <higgins.christopher.j@gmail.com>
2018-08-17 23:45:49 -0400

    Initial upload

---
+++ test_decode.py
@@ -0,0 +1,14 @@
+import base64
+print "Select input encoding:"
+print "A: Base64"
+print "B: Hex"
+print "C: Decimal"
+selection = raw_input()
+#if selection = A
+#    start = raw_input('Pase Base64 string to be decoded: ' )
+#    result = base64.b64decode(start)
+#if selection = B
+#if selection = C
+#start = raw_input('Paste base64 string to be decoded: ' )
+#result = base64.b64decode(start)
+print result
---
+++ testdecompress.py
@@ -0,0 +1,6 @@
+#Test Decompress
+import base64
+coded_string = ""
+base64.b64decode(coded_string)
+print coded_string
+input()
