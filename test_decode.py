57195120dbfd843d1f586902b84f0c5f6b41f1ba
BuriedButBreathing <higgins.christopher.j@gmail.com>
2018-08-19 16:45:02 -0400

    Expounded on b64 decoding. Still a lot of work to do

--- test_decode.py
+++ test_decode.py
@@ -1,9 +1,14 @@
+# Add in decoding modules!!
+# Comment things!!
+
 import base64
 print('Select which encoding the input string is in: \nA: Base64\nB: Hex\nC: Decimal\nD: Binary')
 selection = str(input())
 print('Your selection was: ' + (selection))
 if selection in ['a', 'A']:
-    print('Base64')
+    b64string = str(input('Paste Base64 string to be decoded: '))
+    base64.b64decode(b64string)
+    print(b64string)
 elif selection in ['B', 'b']:
    print("Hex")
 elif selection in ['C', 'c']:
