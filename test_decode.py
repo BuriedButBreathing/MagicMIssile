15a7aabfc1c1fd9184b82649c06932e0437ec518
BuriedButBreathing <higgins.christopher.j@gmail.com>
2018-08-20 10:51:09 -0400

    Got everything but decimal and binary working.

--- test_decode.py
+++ test_decode.py
@@ -2,17 +2,23 @@
 # Comment things!!
 
 import base64
+import codecs
 print('Select which encoding the input string is in: \nA: Base64\nB: Hex\nC: Decimal\nD: Binary')
 selection = str(input())
 print('Your selection was: ' + (selection))
 if selection in ['a', 'A']:
     b64string = str(input('Paste Base64 string to be decoded: '))
-    base64.b64decode(b64string)
-    print(b64string)
+    decodedb64 = base64.b64decode(b64string)
+    print(decodedb64)
 elif selection in ['B', 'b']:
-   print("Hex")
+    hexstring = str(input('Paste Hex string to be decoded: '))
+    decodedhex = codecs.decode(hexstring, "hex").decode('utf-8')
+    print(decodedhex)
 elif selection in ['C', 'c']:
-    print("Decimal")
+    decstring = str(input('Paste decimal string to be decoded: '))
+    decstringint = int(decstring)
+    decodedec = chr(decstringint)
+    print(decodedec)
 elif selection in ['D', 'd']:
     print("Binary")
 else:
