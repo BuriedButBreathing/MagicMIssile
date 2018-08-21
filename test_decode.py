5eaa68cc4762eb1b8c86a9a7c05f04a4bc3e7f62
BuriedButBreathing <higgins.christopher.j@gmail.com>
2018-08-21 13:54:40 -0400

    Almost working

--- test_decode.py
+++ test_decode.py
@@ -1,25 +1,46 @@
 # Add in decoding modules!!
 # Comment things!!
-
+#Library imports
 import base64
 import codecs
+#Decode type select
 print('Select which encoding the input string is in: \nA: Base64\nB: Hex\nC: Decimal\nD: Binary')
 selection = str(input())
 print('Your selection was: ' + (selection))
+#Base64 Decoding
 if selection in ['a', 'A']:
     b64string = str(input('Paste Base64 string to be decoded: '))
     decodedb64 = base64.b64decode(b64string)
     print(decodedb64)
+# Hex Decoding
 elif selection in ['B', 'b']:
     hexstring = str(input('Paste Hex string to be decoded: '))
     decodedhex = codecs.decode(hexstring, "hex").decode('utf-8')
     print(decodedhex)
+# Decimal Decoding
 elif selection in ['C', 'c']:
-    decstring = str(input('Paste decimal string to be decoded: '))
-    decstringint = int(decstring)
-    decodedec = chr(decstringint)
-    print(decodedec)
+    print('Paste decimal string to be decoded')
+    decstring = input().split()
+    decoutput = []
+    for x in decstring:
+        print(x)
+        decstringint = int(x)
+        decoutput.append(chr(decstringint))
+    print("".join(decoutput))
+# Binary decoding
 elif selection in ['D', 'd']:
     print("Binary")
+    print('Paste Binary string to be decoded: ')
+    bininput =input().split()
+    binoutput = []
+    for x in bininput:
+        print(x)
+        bininput = int(x)
+        binoutput.b2a_uu(bininput)
+    print("".join(binoutput))
+    #boutput = bininput.b2a(bininput)
+    #print (boutput)
+    #binoutput = bininput.b2a_uu(data)
+    #print(binoutput)
 else:
     print("Choose again, but choose correctly this time")
